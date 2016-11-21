20762bc66804cfddba00928df5e25ec1450fcabf
giagiar <giagiar.x@gmail.com>
2016-11-21 17:00:44 +0800

    Add files via upload

--- function.js
+++ function.js
@@ -9,6 +9,23 @@ function openWindow(id) {
 } 
 
 
+function change(TabID) 
+{ 
+    var nodes = document.getElementById('subtitle').getElementsByTagName('a'); 
+ 
+    for (var i = 0; i < nodes.length; i++) { 
+        nodes[i].className = 'btn-none';
+    } 
+    document.getElementById(TabID).className='btn-none-active';
+} 
+
+function Load(Tab){
+ if (window.parent['change']){
+  window.parent.change(Tab);
+ }
+}
+
+
 function clearcolor(x, y){
     document.getElementById(x).style.opacity=1.0;
     document.getElementById(y).className='yellow-back';
@@ -17,6 +34,22 @@ function clearcolor(x, y){
 function blurcolor(x, y){
     document.getElementById(x).style.opacity=0.7;
     document.getElementById(y).className='blue-back';
+}
+
+
+var page;
+
+function contentChoose(pagenum){
+    page=pagenum;
+}
+
+function content(){
+    if (page==1)
+        document.getElementById('subpageContent').src="experience.html";
+    else if (page==2)
+        document.getElementById('subpageContent').src="work.html";
+    else if (page==3)
+        document.getElementById('subpageContent').src="life.html";
 }
 
  
